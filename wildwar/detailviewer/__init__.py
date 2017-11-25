b3c7ce1daa7932905a5f684b4e9290d35c2e26a1
GottaDiveIntoPython <flow4re2c@gmail.com>
2017-11-25 22:41:48 +0900

    ファイル名の変更

--- wildwar/detailviewer/__init__.py
+++ wildwar/detailviewer/__init__.py
@@ -1,4 +1,4 @@
 # -*- coding: utf-8 -*-
 
-from .unitcard import UnitPrototypeDetailViewer
-from .spellcard import SpellPrototypeDetailViewer
+from .unitprototype import UnitPrototypeDetailViewer
+from .spellprototype import SpellPrototypeDetailViewer
